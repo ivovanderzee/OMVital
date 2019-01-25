14b95ccc27328753ceab6e890bf15f6ec3db3220
Tim <timvdzee@live.nl>
2019-01-25 16:37:37 +0100

    Eerste frontend stappen.

--- index.php
+++ index.php
@@ -1,30 +1,65 @@
-<?php
-
-require 'Backend/blogQuery.php';
-
-?>
-
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <title>OMVital</title>
+  <title>OMVital</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  .navbar{
+  	margin-bottom:0;
+  	border-radius: 0;
+  	background-color: transparent;
+  	padding: 1% 0;
+  	font-size: 1.2em;
+  }
+
+  .navbar-brand{
+  	float:left;
+  	min-height: 30px;
+  	padding: 0 15px 5px;
+  }
+
+  .navbar-inverse .navbar-nav .active a,
+  .navbar-inverse .navbar-nav .active a:focus,
+  .navbar-inverse .navbar-nav .active a:hover {
+	color: rgb(81,141,63);
+	background-color: transparent;
+  }
+
+ 
+
+  </style>
 </head>
-
 <body>
+<nav class="navbar navbar-inverse" style="border-bottom: 1vh solid rgb(81,141,63);">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#"><img src="img/OMVital_logo.png"></a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+		<ul class="nav navbar-nav navbar right" style=":hover{color:rgb(81,141,63);}">
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#">Over OMVital</a></li>
+			<li><a href="#">Openingstijden</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+		</div>
+	</div>
+</nav>
 
 
 
 
-    <?php foreach($pages as $page): ?>
 
-    <?php echo $page['naam'];?>
-
-    <?php echo $page['tekst'];?>
-
-    <?php endforeach; ?>
 
 </body>
-
 </html>
